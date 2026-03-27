9289b212383d901ae01200b13872d33a738f1e9e
jasim28v-cloud <jasim28v@gmail.com>
2026-03-27 16:15:42 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getDatabase, ref, push, set, onValue, update, get, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-
 const firebaseConfig = {
     apiKey: "AIzaSyBMKBnXRZ2mL5C2FYRih6NSpBIbebYeTBI",
     authDomain: "porn-dc411.firebaseapp.com",
@@ -9,15 +5,16 @@ const firebaseConfig = {
     projectId: "porn-dc411",
     storageBucket: "porn-dc411.firebasestorage.app",
     messagingSenderId: "344117985586",
-    appId: "1:344117985586:web:d1064a4fd269983174572b"
+    appId: "1:344117985586:web:d1064a4fd269983174572b",
+    measurementId: "G-CZ6E0V68WZ"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
-
-export { ref, push, set, onValue, update, get, child };
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
 
 // Cloudinary Configuration
-export const CLOUD_NAME = 'dnillsbmi';
-export const UPLOAD_PRESET = 'ekxzvogb';
+const CLOUD_NAME = 'dnillsbmi';
+const UPLOAD_PRESET = 'ekxzvogb';
+
+console.log('✅ instagrami Ready');
