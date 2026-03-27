8fb4b1179605d268df203805b1b161f3d603a57a
jasim28v-cloud <jasim28v@gmail.com>
2026-03-27 13:12:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getDatabase, ref, push, set, onValue, update, get, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMKBnXRZ2mL5C2FYRih6NSpBIbebYeTBI",
-  authDomain: "porn-dc411.firebaseapp.com",
-  databaseURL: "https://porn-dc411-default-rtdb.firebaseio.com",
-  projectId: "porn-dc411",
-  storageBucket: "porn-dc411.firebasestorage.app",
-  messagingSenderId: "344117985586",
-  appId: "1:344117985586:web:d1064a4fd269983174572b",
-  measurementId: "G-CZ6E0V68WZ"
+    apiKey: "AIzaSyBMKBnXRZ2mL5C2FYRih6NSpBIbebYeTBI",
+    authDomain: "porn-dc411.firebaseapp.com",
+    databaseURL: "https://porn-dc411-default-rtdb.firebaseio.com",
+    projectId: "porn-dc411",
+    storageBucket: "porn-dc411.firebasestorage.app",
+    messagingSenderId: "344117985586",
+    appId: "1:344117985586:web:d1064a4fd269983174572b"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const db = getDatabase(app);
+
+export { ref, push, set, onValue, update, get, child };
+
+// Cloudinary Configuration
+export const CLOUD_NAME = 'dnillsbmi';
+export const UPLOAD_PRESET = 'ekxzvogb';
