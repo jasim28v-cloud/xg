4819d898509774803627b76cd0053a5e9ea77d7c
jasim28v-cloud <jasim28v@gmail.com>
2026-03-30 01:42:33 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,19 @@
-// Firebase Configuration - استخدم هذه الإعدادات الجديدة
 const firebaseConfig = {
-    apiKey: "AIzaSyBMKBnXRZ2mL5C2FYRih6NSpBIbebYeTBI",
-    authDomain: "porn-dc411.firebaseapp.com",
-    databaseURL: "https://porn-dc411-default-rtdb.firebaseio.com",
-    projectId: "porn-dc411",
-    storageBucket: "porn-dc411.firebasestorage.app",
-    messagingSenderId: "344117985586",
-    appId: "1:344117985586:web:d1064a4fd269983174572b",
-    measurementId: "G-CZ6E0V68WZ"
+    apiKey: "AIzaSyD7Cf_VMH1ACx1eweozlF9D26yw-pj9WcY",
+    authDomain: "gorm-b3316.firebaseapp.com",
+    databaseURL: "https://gorm-b3316-default-rtdb.firebaseio.com",
+    projectId: "gorm-b3316",
+    storageBucket: "gorm-b3316.firebasestorage.app",
+    messagingSenderId: "1092629474101",
+    appId: "1:1092629474101:web:a4f94a6af1c70a2f3efa7c",
+    measurementId: "G-BL0B7VHHV8"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
 
-// Cloudinary Configuration - استخدم هذه الإعدادات
 const CLOUD_NAME = 'dnillsbmi';
 const UPLOAD_PRESET = 'ekxzvogb';
 
-console.log('✅ ECHO Platform Ready');
+console.log('✅ instagrami Ready');
